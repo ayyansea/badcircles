a91d3fcb98ca884bb9741a7a549d1b51bef88e18
ayyansea <ayyansea@gmail.com>
2022-04-02 19:38:41 +0300

    feat: добавлены паттерны приветствий []

--- badcircles/patterns.py
+++ badcircles/patterns.py
@@ -8,7 +8,13 @@ class MemePatterns(Enum):
         ["^.*brain power.*$", "^.*брейн павер.*$"],
         "O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A-JO-ooo-oo-oo-oo EEEEO-A-AAA-AAA",
     ]
+    BELLYBUTTON = [["^.*пуп[к|о][кеиа|ов|ом|ам(и)?|ах]*$"], "где мое перо?"]
 
 
 class ReplyPatterns(Enum):
     ONEMISS = [["^.*Hitcounts: (\d+\/)+1.*$"], "1 miss rip bruh"]
+
+
+class GreetingPatterns(Enum):
+    PRIVET = [['^прив(яу)?(ет)?(ик|ки)?и?$'], 'привет привет']
+    ZDAROVA = [['^здарова$'], 'здарова']
