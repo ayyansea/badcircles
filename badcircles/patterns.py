bad7695f1209f91f83eb4d219af00286f5594468
ayyansea <ayyansea@gmail.com>
2022-04-02 16:42:57 +0300

    feat: добавлен набор паттернов для ответов на сообщения []

--- badcircles/patterns.py
+++ badcircles/patterns.py
@@ -2,9 +2,13 @@ from enum import Enum
 
 
 class MemePatterns(Enum):
-    BLUEZENITH = [["^.*727.*$"], "WHEN YOU FUCKING SEE IT"]
+    BLUEZENITH = [["^.*727.*$"], "WYSI"]
     AZER = [["^.*azer.*$", "^.*азер.*$"], "Azer isn't so great? Are you kidding me?"]
     BRAINPOWER = [
         ["^.*brain power.*$", "^.*брейн павер.*$"],
         "O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A-JO-ooo-oo-oo-oo EEEEO-A-AAA-AAA",
     ]
+
+
+class ReplyPatterns(Enum):
+    ONEMISS = [["^.*Hitcounts: (\d+\/)+1.*$"], "1 miss rip bruh"]
