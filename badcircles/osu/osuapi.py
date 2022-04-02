791acd31d578431f3cd1879fffd54a7a7dffb6b9
ayyansea <ayyansea@gmail.com>
2022-04-02 21:25:04 +0300

    fix: пофикшен импорт []

--- badcircles/osu/osuapi.py
+++ badcircles/osu/osuapi.py
@@ -1,8 +1,4 @@
-import sys
-
-sys.path.append("..")
-
 from ossapi import OssapiV2
-from settings import OSU_CLIENT, OSU_REDIRECT_URI, OSU_TOKEN
+from ..settings import OSU_CLIENT, OSU_REDIRECT_URI, OSU_TOKEN
 
 api = OssapiV2(OSU_CLIENT, OSU_TOKEN, OSU_REDIRECT_URI)
